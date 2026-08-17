4757b3930e09723c092e34f1d8e7e2d2a5c95051
Gui6456 <guiborgia@gmail.com>
2026-08-17 16:55:53 -0300

    atividades diversas com javascript

---
+++ 03_atividades.js
@@ -0,0 +1,110 @@
+// -- Atividade 0
+console.log("  _____ ")
+console.log(" |     |")
+console.log(" |     |")
+console.log(" |     |")
+console.log(" |     |")  
+console.log("  ‾‾‾‾‾ ")
+
+// -- Atividade 1
+
+let cidade = "São Carlos"
+let estado = "São Paulo"
+
+console.log("Eu moro em " +cidade + " - " + estado + "\n")
+
+// -- Atividade 2
+
+let numero1 = 2
+let numero2 = 7
+let soma = numero1 + numero2
+
+console.log("A soma dos numéros é: "+soma + "\n")
+
+// -- Atividade 3
+
+let idade = 67
+let envelhecer = idade - 10
+
+console.log("Olá, você tem " +idade+ " e daqui 10 anos terá "+ envelhecer + "\n")
+
+// -- Atividade 4
+
+let numero = 10 
+let dobro = numero * 2
+let triplo = numero * 3
+
+console.log("O número é " +numero+ ", seu dobro é " +dobro+ " e seu triplo é " +triplo+ "\n")
+
+// -- Atividade 5
+
+let produto1 = "garrafa"
+let preco_produto1 = 13
+let produto2 = "lapis"
+let preco_produto2 = 4.90
+let total_produtos = preco_produto1 + preco_produto2
+
+console.log("Estamos vendendo uma "+produto1+ " no valor de "+preco_produto1)
+console.log("Assim como também estamos vendendo um "+produto2+ " no valor de "+preco_produto2)
+console.log("O total de ambos os produtos é R$ "+total_produtos+ " reais\n")
+
+// -- Atividade 6
+
+let salario = 3000
+let aumento = 500
+let desconto_INSS = -250
+let novo_salario = salario + aumento + desconto_INSS
+
+console.log("O salário atual é de R$ " +salario+ " e o novo salário com base nos novos valores está avaliado em R$ "+novo_salario+ " reais\n")
+
+// -- Atividade 7
+
+let mesada = 100 
+let gasto = 35
+let sobrou = mesada - gasto
+
+console.log("Joãozinho tem uma mesada de R$"+mesada+ " e gastou R$"+gasto+ " da sua mesada sobrou R$"+sobrou+ " reais\n")
+
+// -- Atividade 8
+
+let base = 10
+let altura = 20
+let area_retangulo = base * altura
+
+console.log("A base desse retangulo é de "+base+ "cm, sua altura é de "+altura+ "cm e sua área é de "+area_retangulo+ "m^2 \n")
+
+// -- Atividade 9
+
+let celsius = 32
+let fahrenheit = celsius * 1.8 + 32 
+let kelvin = celsius + 273
+
+console.log(celsius+ "°C equivalem à: "+fahrenheit+ "°F e " +kelvin+ "K \n")
+
+// -- Atividade 10
+
+let horas = 4
+let minutos = horas * 60
+let segundos = minutos * 60
+
+console.log("São "+horas+ "h disponíveis, "+minutos+ "m e " +segundos+ "s \n")
+
+// -- Atividade 11
+
+let variavel_x = "x"
+let variavel_y = "y"
+variavel_y = variavel_x
+
+console.log("Existem dois valores, "+variavel_x+ " e "+variavel_y+ "\n")
+
+// -- Atividade 12
+
+let distancia = 150
+let qnt_litros = 300
+let preco_combustivel = 7.00
+let calculo_distancia = distancia / qnt_litros
+let calculo_combustivel = qnt_litros * preco_combustivel
+
+console.log("A distância percorrida foi de "+distancia+ "km, foi usado "+qnt_litros+
+    "L, o carro consumiu "+calculo_distancia+ "L na viagem e o valor total gasto de combustível foi de R$"+ calculo_combustivel+ " reais" )
+
